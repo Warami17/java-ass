3c9d38508eec440250d25ab1bd5a37bf22948b61
Warami17 <143951724+Warami17@users.noreply.github.com>
2023-10-08 20:05:04 +0100

    Add files via upload

---
+++ javaScript2/java.js
@@ -0,0 +1,82 @@
+//object for 5 students
+
+const studentOne={
+firstName:"rami",
+lastName: "omj",
+phoneNo: "08122189631",
+address: "choba",
+age:15,
+gender: "female",
+email: "smarthubMail",
+score: 159
+}
+
+console.log(studentOne)
+
+const studentTwo={
+    firstName:"success",
+    lastName: "daniel",
+    phoneNo: "0812346791",
+    address: "aluu",
+    age:17,
+    gender: "male",
+    email: "smarthubMail",
+    score: 200
+    }
+
+    console.log(studentTwo)
+
+    const studentThree={
+        firstName:"faith",
+        lastName: "peters",
+        phoneNo: "0812578901",
+        address: "rumosi",
+        age:15,
+        gender: "female",
+        email: "smarthubMail",
+        score: 213
+        }
+
+console.log(studentThree)
+
+        const studentFour={
+            firstName:"precious",
+            lastName: "dinel",
+            phoneNo: "0708929631",
+            address: "location",
+            age:18,
+            gender: "male",
+            email: "smarthubMail",
+            score: 180
+            }
+
+console.log(studentFour)
+
+            const studentFive={
+                firstName:"saviour",
+                lastName: "lawrence",
+                phoneNo: "08122189631",
+                address: "alakahia",
+                age:16,
+                gender: "female",
+                email: "smarthubMail",
+                score: 300
+                }
+console.log(studentFive)
+
+//array
+
+const singers=["davido", "wizkid", "rami", "maggix", "rema", "ayra",
+"future", "burnaboy", "larrygaga","imole","iceprince","asa", "tems",
+]
+
+console.log(singers)
+
+
+//if or else statement
+
+let answer= 7
+
+if (answer>7) {console.log("passed")}
+else if (answer<7) {console.log("failed")}
+else if (answer=7) {console.log("excellent")}
